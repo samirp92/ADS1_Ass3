c9ede207f085cc4910e7b8d16d3cbe4e7e141d03
Samirkumar K. Patel <117090920+samirp92@users.noreply.github.com>
2023-01-18 20:33:36 +0000

    k-means clustering algorithm added

--- Assignment-3_Clustering and fitting_Student ID-21072112.py
+++ Assignment-3_Clustering and fitting_Student ID-21072112.py
@@ -69,3 +69,60 @@ df = dataframe[features].copy()  # To store the value of selected indicator only
 df.to_excel('df.xlsx')  # To save the data
 df = norm_df(df)  # Convert the data into normalized form
 X = df.values  # To store the data as values
+
+# Find the WCSS to find the correct cluster number for kmeans clustering
+wcss = []
+for i in range(1,11):
+    kmeans = KMeans(n_clusters=i, init='k-means++', random_state = 0)
+    kmeans.fit(X)
+    wcss.append(kmeans.inertia_)
+    
+#  Plot an elbow graph
+plt.figure(dpi =300)
+plt.plot(range(1,11), wcss)
+plt.xlabel('Number of Clusters', fontsize = 12)
+plt.ylabel('WCSS', fontsize = 12)
+plt.title('The Elbow Point Graph', fontsize = 15)
+plt.show()
+
+# To find the silhouette_score for number of cluster
+print('Silhouette score')
+for ic in range(2, 7):
+    # set up kmeans and fit
+    kmeans = cluster.KMeans(n_clusters=ic)
+    kmeans.fit(X)
+    # extract labels and calculate silhoutte score
+    labels = kmeans.labels_
+    print(ic, skmet.silhouette_score(X, labels))
+   
+kmeans = KMeans(n_clusters = 3, init = 'k-means++', random_state = 0)
+
+# Return a label for each data point based on their cluster
+Y = kmeans.fit_predict(X)
+
+# Extract labels and cluster centres
+labels = kmeans.labels_
+cen = kmeans.cluster_centers_
+
+# plotting all the clusters and their Centroids
+plt.figure(figsize=(8,8), dpi =300)
+plt.scatter(X[Y==0,0], X[Y==0,1], s = 75, c = 'green', label = 'Cluster 0')
+plt.scatter(X[Y==1,0], X[Y==1,1], s = 75, c = 'red', label = 'Cluster 1')
+plt.scatter(X[Y==2,0], X[Y==2,1], s = 75, c = 'yellow', label = 'Cluster 2')
+
+# Plot the centroids
+plt.scatter(kmeans.cluster_centers_[:,0], kmeans.cluster_centers_[:,1], s = 100, c = 'blue', label = 'Centroids')
+plt.title('Clusters (Urban population vs GDP growth)', fontsize = 20)
+plt.xlabel('Urban population (% of total population) in 2020', fontsize = 18)
+plt.ylabel('GDP growth (annual %) in 2020', fontsize = 18)
+plt.legend(bbox_to_anchor = (1.0, 1.0), fontsize = 18, loc = 'upper left')
+plt.show()
+
+# Extract & Store the group of country according to labels
+country_group0 = dataframe[labels == 0]
+country_group1 = dataframe[labels == 1]
+country_group2 = dataframe[labels == 2]
+
+country_group0.to_excel('country_group0.xlsx')
+country_group1.to_excel('country_group1.xlsx')
+country_group2.to_excel('country_group2.xlsx')
